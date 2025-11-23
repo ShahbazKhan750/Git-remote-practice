4120986aa829cb99466787315708d5c5af400515
MD Shahbaz Khan <kmdshahbaz121@gmail.com>
2025-11-24 00:42:04 +0530

    new feature added

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ function greet(userName) {
 }
 console.log(greet("Alice"));
 
+function above18(age){
+    if(age>18){
+        console.log(`you are eligible for voting ${name}`);
+    }
+}
+
+function table(num){
+    for(let i=1;i<=10;i++){
+        console.log(`${num} x ${i} = ${num*i}`);
+    }   
+}
+table(5);

9cfabb14daf9aa2cc8dcc3402382b5bec6ab3a26
MD Shahbaz Khan <kmdshahbaz121@gmail.com>
2025-11-23 18:50:19 +0530

    Initial code added

---
+++ index.js
@@ -0,0 +1,9 @@
+const age=24;
+console.log(`I am ${age} years old.`);
+const name="John Doe";
+console.log(`My name is ${name}.`);
+
+function greet(userName) {
+    return `Hello, ${userName}!`;
+}
+console.log(greet("Alice"));
